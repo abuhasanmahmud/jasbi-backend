5ce666509d578fddc163ab20a0f6f8e57c54b037
abuhasanmahmud <mahmud.me420@gmail.com>
2023-08-17 09:15:04 +0600

    Feat(V-1) for deploy on versel

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb://kachabazar:pjsWv7FsQ1tbMa6xkachabazarfsdfsdaf@cluster0-shard-00-00.zlt6h.mongodb.net:27017,cluster0-shard-00-01.zlt6h.mongodb.net:27017,cluster0-shard-00-02.zlt6h.mongodb.net:27017/ecommerce?ssl=true&replicaSet=atlas-a64arb-shard-0&authSource=admin&retryWrites=true&w=majority"
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`mongoDb connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
